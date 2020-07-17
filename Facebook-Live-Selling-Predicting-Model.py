af89f4b1ab023dc8eb18f2cfa343190bb9639f0f
guneshprasads <guneshprasads2506@gmail.com>
2020-07-17 20:14:10 +0530

    Created the Hours Coloumn

--- Facebook-Live-Selling-Predicting-Model.py
+++ Facebook-Live-Selling-Predicting-Model.py
@@ -3,3 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#importing the datasets
+dataset = pd.read_csv('Live.csv')
+
+dataset['status_published'] = pd.to_datetime(dataset.status_published)
+dataset.dtypes
+dataset.insert(2, "Hours", dataset.status_published.dt.time, True)
+
+#Creating the another dataset and Filtering the data Form the dataset
