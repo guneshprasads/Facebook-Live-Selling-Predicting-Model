c9a468a0317c929207292a912a9480424ee59acc
guneshprasads <guneshprasads2506@gmail.com>
2020-07-20 21:09:13 +0530

    Removed the Duplicates Values

--- Facebook-Live-Selling-Predicting-Model.py
+++ Facebook-Live-Selling-Predicting-Model.py
@@ -6,3 +6,17 @@ import pandas as pd
 #importing the datasets
 dataset = pd.read_csv('Live.csv')
 
+#lets see the small part of the dataset
+dataset.head(2)
+
+#there is no use of last four coloumns
+dataset=dataset.drop(dataset.columns[[-1,-2,-3,-4]],axis=1)
+
+#lets find the count unique values 
+dataset.info()
+dataset.nunique()
+
+#Remove the Duplicate Values 
+duplicated_data = dataset[dataset['status_id'].duplicated() == True]
+dataset1 = dataset.drop_duplicates(subset='status_id',keep='last')
+
